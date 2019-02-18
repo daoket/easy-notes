e35b289008f56cf57cafbaeed58bd9ef8e36204f
daoket <daoket@163.com>
2019-02-18 12:53:02 +0800

    :tada:

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Easy-Notes
+
 Easiest way for taking quick notes.
+
+![](img/2019-02-18-12-51-37.png)
+
+Easy Notes - A perfect tool for taking quick notes. Its fast, secure, and private. A must tool for everyone. 
---
+++ popup.js
@@ -0,0 +1,8 @@
+﻿var textarea = document.getElementById("note")
+var cacheVal = localStorage.getItem('chromeNotePluginVal')
+textarea.value = cacheVal
+
+textarea.addEventListener('input', function (val) {
+  var val = textarea.value
+  localStorage.setItem('chromeNotePluginVal', val)
+})
